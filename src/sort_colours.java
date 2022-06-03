e7cada80dc32e172d0b9402b34c75717a6631262
HP <shivangi.agrawal_cs.aiml20@gla.ac.in>
2022-06-03 17:50:36 +0530

    public

--- src/sort_colours.java
+++ src/sort_colours.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class sort_colours {
+    public void sorted(int[] arr){
+        for(int i=0;i< arr.length;i++){
+            int temp= arr[i];
+            int j=i;
+            while (j>0 && arr[j-1]>temp){
+                arr[j]=arr[j-1];
+                j--;
+                arr[j]=temp;
+            }
+        }
+    }
+}
+
+class M{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        sort_colours stc= new sort_colours();
+        stc.sorted(arr);
+        for (int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
 }
